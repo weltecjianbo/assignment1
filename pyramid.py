85fa058872d614b602db729cc0d87941671dc4dd
jianbolinz <lee.li01@student.weltec.ac.nz>
2022-04-18 17:56:43 +1200

    committing pyramid.py to local repo

--- pyramid.py
+++ pyramid.py
@@ -9,7 +9,7 @@ def pypart(n):
         for j in range(0, i+1):
          
             # printing stars
-            print("^ ",end="")
+            print("* ",end="")
       
         # ending line after each row
         print("\r")
